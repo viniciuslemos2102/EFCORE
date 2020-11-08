d3c17eed9ea2c4b935fc250f3fd68893f6b3635f
viniciuslemos2102 <viniciuslemoscontato21@gmail.com>
2020-11-08 15:25:49 -0300

    dethalhes pronto

--- Controllers/TesteController1.cs
+++ Controllers/TesteController1.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using EFCORE.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCORE.Controllers
 {
@@ -40,5 +41,99 @@ namespace EFCORE.Controllers
             }
             return View(aluno);
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+            if(aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //post edit
+        public IActionResult Edit(int id,[Bind("Id,Nome,Sexo,Email,Nascimento")]Aluno aluno)
+        {
+            if(id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    _context.SaveChanges();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if(!AlunoExist(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+
+            if(aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+        
+        private bool AlunoExist(int id)
+        {
+            return _context.Alunos.Any(e => e.Id == id);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirma(int? id)
+        {
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+            _context.Alunos.Remove(aluno);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+
+            if(aluno==null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
     }
+
 }
